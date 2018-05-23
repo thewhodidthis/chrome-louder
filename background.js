8228a6203314349fcf16d6c6adaabbf2404ce6ef
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-05-23 13:25:26 +0100

    initial

---
+++ background.js
@@ -0,0 +1,46 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext
+
+const louder = {}
+
+chrome.browserAction.onClicked.addListener(({ audible, id }) => {
+  // Skip if tab capture unavailable
+  if (!audible) {
+    return
+  }
+
+  if (louder.audio && louder.audio.state === 'running') {
+    // Revert
+    louder.fader.gain.setTargetAtTime(1.0, louder.audio.currentTime + 0.25, 0.5)
+    chrome.browserAction.setIcon({ tabId: id, path: 'assets/icon.png' })
+
+    // Prep for next click
+    louder.fader.disconnect()
+    louder.input.mediaStream.getAudioTracks().pop().stop()
+    louder.audio.close()
+
+    return
+  }
+
+  chrome.tabCapture.capture({ audio: true }, (stream) => {
+    if (stream) {
+      const audio = new AudioContext()
+
+      const input = audio.createMediaStreamSource(stream)
+      const fader = audio.createGain()
+
+      input.connect(fader)
+      fader.connect(audio.destination)
+
+      // Boost volume
+      fader.gain.setTargetAtTime(3.0, audio.currentTime + 0.25, 0.25)
+
+      // Replace icon
+      chrome.browserAction.setIcon({ tabId: id, path: 'assets/icon-x.png' })
+
+      // Update locals
+      Object.assign(louder, { audio, input, fader })
+    } else {
+      console.log(chrome.runtime.lastError)
+    }
+  })
+})
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Louder
+> Web audio based gain pot for Chrome / [Add to Chrome](https://chrome.google.com/webstore/detail/louder/ifeendclfjoecpdjdckmimhfcmkadhnp) &rarr;
